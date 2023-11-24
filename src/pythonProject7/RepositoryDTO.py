941a14c1dec016d28a34d3ca6c2586252d8437a1
yuliiaaaaa <105826111+yuliiaaaaa@users.noreply.github.com>
2023-11-24 09:44:13 +0200

    Delete src/pythonProject7 directory

--- src/pythonProject7/RepositoryDTO.py
+++
@@ -1,9 +0,0 @@
-class RepositoryDTO:
-    def __init__(self, id, name, vcs_type, url):
-        self.id = id
-        self.name = name
-        self.vcs_type = vcs_type
-        self.url = url
-
-    def __str__(self):
-        return f"RepositoryDTO(id={self.id}, name={self.name}, type={self.type}, url={self.url})"
